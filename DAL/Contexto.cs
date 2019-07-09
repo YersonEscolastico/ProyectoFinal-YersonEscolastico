38173b18fe177bc899c8518370afd2764206aaf2
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-07-09 13:37:52 +0800

    Agregando entidad Usuarios al contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
