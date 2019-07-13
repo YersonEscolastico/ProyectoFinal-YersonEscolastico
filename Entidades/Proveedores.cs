2fd9b6b2c72f32f15fa5c16174e5d4d9c3de5271
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-07-14 00:56:00 +0800

    Agregando entidad Proveedores

---
+++ Entidades/Proveedores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Proveedores
+    {
+        [Key]
+       public int ProveedorId { get; set; }
+       public string Nombres { get; set; }
+       public string Telefono { get; set; }
+       public string Celular { get; set; }
+       public string Email { get; set; }
+       public string Direccion { get; set; }
+       public DateTime FechaRegistro { get; set; }
+
+        public Proveedores()
+        {
+            ProveedorId = 0;
+            Nombres = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Email = string.Empty;
+            Direccion = string.Empty;
+            FechaRegistro = DateTime.Now;
+        }
+    }   
+}
