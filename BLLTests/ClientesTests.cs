28a5b6e72e288087860eed1050df3c1b54d37edd
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-07-13 12:17:35 +0800

    Haciendo Tests de Clientes

---
+++ BLLTests/ClientesTests.cs
@@ -0,0 +1,82 @@
+﻿using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class ClientesTest
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Clientes cliente = new Clientes()
+            {
+                ClienteId = 1,
+                Nombres = "Yesica",
+                Sexo = "F",
+                Email = "Yesica@gmail.com",
+                Cedula = "0",
+                Direccion = "g",
+                Telefono = "8092145",
+                Celular = "829213",
+                FechaNacimiento = DateTime.Now,
+                FechaRegistro = DateTime.Now
+            };
+
+            Assert.IsTrue(db.Guardar(cliente));
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Clientes cliente = new Clientes()
+            {
+                ClienteId = 1,
+                Nombres = "Yesicaa",
+                Sexo = "F",
+                Email = "Yesica@gmail.com",
+                Cedula = "0",
+                Direccion = "g",
+                Telefono = "8092145",
+                Celular = "829213",
+                FechaNacimiento = DateTime.Now,
+                FechaRegistro = DateTime.Now
+            };
+
+            Assert.IsTrue(db.Modificar(cliente));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsNotNull(db.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsNotNull(db.GetList(t => true));
+        }
+    }
+}
