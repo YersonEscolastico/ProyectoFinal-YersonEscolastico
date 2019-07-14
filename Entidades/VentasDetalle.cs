42c7ed77a2ce401efb12d447c5eda3e6044ab7f7
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-07-14 22:32:42 +0800

    Agregando entidad VentasDetalle

---
+++ Entidades/VentasDetalle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class VentasDetalle
+    {   
+        public int VentasDetalleID { get; set; }
+        public int VentaId { get; set; }
+        public int VehiculoId{ get; set; }
+        public int Precio { get; set; }
+
+
+        public VentasDetalle()
+        {
+            VentasDetalleID = 0;
+            VentaId = 0;
+            VehiculoId = 0;
+            Precio = 0;
+        }
+
+        public VentasDetalle(int ventasDetalleID, int ventaId, int vehiculoId, int precio)
+        {
+            VentasDetalleID = ventasDetalleID;
+            VentaId = ventaId;
+            VehiculoId = vehiculoId;
+            Precio = precio;
+        }
+    }
+}
