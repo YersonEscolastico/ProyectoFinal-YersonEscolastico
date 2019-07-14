97e8bbff54874b78ac78689cde864c936c3dabe6
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-07-14 10:40:11 +0800

    Agregando Entidad Ventas

---
+++ Entidades/Ventas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ventas
+    {
+        public int VentaId { get; set; }
+        public decimal Precio { get; set; }      
+        public DateTime FechaVenta { get; set; }
+
+
+        public Ventas()
+        {
+            VentaId = 0;
+            Precio = 0;
+            FechaVenta = DateTime.Now;
+        }
+    }
+}
